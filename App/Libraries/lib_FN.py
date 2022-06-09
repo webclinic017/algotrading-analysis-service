60e39704c81fdc55ca90066451c1e724ed1098a1
parag-b <parag+github@barangale.in>
2022-06-09 14:06:54 +0530

    research framework creation

--- App/Libraries/lib_FN.py
+++ App/Libraries/lib_FN.py
@@ -1,3 +1,32 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
 def getORB(Xdf):
     ORBdf = Xdf.between_time('09:15', '09:30')
     return ORBdf["low"].min(), ORBdf["high"].max()
+
+
+def getDates(duration, end_date):
+
+    base = duration.split(" ")
+    numPart = int(base[0])
+    unit = base[1]
+
+    if end_date == "":
+        end = datetime.date.today()
+    else:
+        end = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+
+    if unit == "day" or unit == "days":
+        start = end - relativedelta(days=numPart)
+    elif unit == "week" or unit == "weeks":
+        start = end - relativedelta(weeks=numPart)
+    elif unit == "mon" or unit == "month" or unit == "months":
+        start = end - relativedelta(months=numPart)
+    elif unit == "year" or unit == "years":
+        start = end - relativedelta(years=numPart)
+    else:
+        print("Invalid duration")
+
+    return start, end
--- research.py
+++ research.py
@@ -5,15 +5,21 @@ import App.DB.tsDB as db
 import App.Engine.Engines as e
 import App.Services.Services as srv
 import App.Strategies.Strategies as s
+import App.Libraries.lib_FN as libFn
 
 env = db.envVar()
 dbConn = db.dbConnect(env)
 
-algo = "S001-01-ORB-OpeningRangeBreakout"
-symbol = "BANKNIFTY-FUT"
+analysis_algorithm = "S001-01-ORB-OpeningRangeBreakout"
+analysis_symbol = "BANKNIFTY-FUT"
+analysis_duration_backward = '3 months'
+analysis_end_date = ""  # "" for today
+
+start, end = libFn.getDates(analysis_duration_backward, analysis_end_date)
+
 # date = "2022-05-20"
 # date = "2022-05-31"
-date = "2022-06-07"
+# date = "2022-06-07"
 
 df = s.execute(env, dbConn, algo + '-entr', symbol, date,
                False)  # get all entry calls
